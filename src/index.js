11cc1692d70b3ba991503e4638533f157562c3ce
danielz0102 <danyzglez@hotmail.com>
2024-12-30 15:37:19 -0600

    Add Todo class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import '@/index.css'
+import { format } from 'date-fns';
+import { toZonedTime } from 'date-fns-tz';
+import { Priority, Todo } from '@/modules/Todo'
+
+function getCurrentDatetime() {
+  const userTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone
+  const currentDate = new Date()
+  const zonedDate = toZonedTime(currentDate, userTimezone)
+  return format(zonedDate, 'dd-MM-yyyy:HH:mm:ss')
+}
+
+const todo = new Todo({
+  title: 'Learn JavaScript',
+  description: 'Learn JavaScript to become a better developer',
+  dueDate: getCurrentDatetime(),
+  priority: Priority.HIGH
+})
+
+console.log(todo)
---
+++ src/modules/Todo.js
@@ -0,0 +1,17 @@
+const Priority = Object.freeze({
+  LOW: 'low',
+  MEDIUM: 'medium',
+  HIGH: 'high'
+})
+
+class Todo {
+  constructor({ title, description, dueDate, priority }) {
+    this.title = title
+    this.description = description
+    this.dueDate = dueDate
+    this.priority = priority
+    this.complete = false
+  }
+}
+
+export { Priority, Todo }
