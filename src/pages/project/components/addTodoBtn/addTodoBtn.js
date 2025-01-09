7172783b715c0e030936af09cdfca03891553640
danielz0102 <danyzglez@hotmail.com>
2025-01-09 16:47:14 -0600

    Refactor add todo button into a component

---
+++ src/pages/project/components/addTodoBtn/addTodoBtn.js
@@ -0,0 +1,29 @@
+import './addTodoBtn.css'
+import addIconSrc from '@/assets/icons/add.svg'
+import { createIcon } from '@/components/icon/icon.js'
+import { Todo } from '@/modules/Todo.js'
+import { updateTodosList } from '../todosList/todosList.js'
+
+export function createAddTodoBtn(project) {
+  const addIcon = createIcon({ src: addIconSrc, alt: 'Plus icon' })
+
+  const addTodoBtn = document.createElement('button')
+  addTodoBtn.id = 'addTodoBtn'
+  addTodoBtn.classList.add('icon-btn', 'icon-btn--rounded')
+  addTodoBtn.appendChild(addIcon)
+
+  addTodoBtn.addEventListener('click', () => handleClick(project))
+
+  return addTodoBtn
+}
+
+function handleClick(project) {
+  project.addTodo(new Todo({
+    title: 'New todo!',
+    description: '',
+    dueDate: 'Today',
+    priority: 'None'
+  }))
+
+  updateTodosList(project)
+}
--- src/pages/project/project.js
+++ src/pages/project/project.js
@@ -1,10 +1,8 @@
 import './project.css'
-import addIconSrc from '@/assets/icons/add.svg'
-import { createIcon } from '@/components/icon/icon.js'
 import { createProjectTitle } from './components/projectTitle/projectTitle.js'
 import { createDeleteProjectBtn } from './components/deleteProjectBtn/deleteProjectBtn.js'
-import { createTodosList, updateTodosList } from './components/todosList/todosList.js'
-import { Todo } from '@/modules/Todo.js'
+import { createTodosList } from './components/todosList/todosList.js'
+import { createAddTodoBtn } from './components/addTodoBtn/addTodoBtn.js'
 
 export function createProjectPage(project) {
   const wrapper = document.createElement('div')
@@ -29,26 +27,4 @@ export function createProjectPage(project) {
   )
 
   return wrapper
-}
-
-function createAddTodoBtn(project) {
-  const addIcon = createIcon({ src: addIconSrc, alt: 'Plus icon' })
-
-  const addTodoBtn = document.createElement('button')
-  addTodoBtn.id = 'addTodoBtn'
-  addTodoBtn.classList.add('icon-btn', 'icon-btn--rounded')
-  addTodoBtn.appendChild(addIcon)
-
-  addTodoBtn.addEventListener('click', () => {
-    project.addTodo(new Todo({
-      title: 'New todo!',
-      description: '',
-      dueDate: 'Today',
-      priority: 'None'
-    }))
-
-    updateTodosList(project)
-  })
-
-  return addTodoBtn
 }
