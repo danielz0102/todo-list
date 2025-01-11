f406cef0d1c85d89bc7174672a73391d9146364b
danielz0102 <danyzglez@hotmail.com>
2025-01-11 17:28:27 -0600

    Remove console log

--- src/modules/consts.js
+++ src/modules/consts.js
@@ -3,6 +3,5 @@ import { addYears } from 'date-fns'
 const currentYear = new Date().getFullYear()
 const DATE_FORMAT = "MMMM dd',' yyyy',' HH:mm 'hrs'"
 const MAX_DATE = addYears(new Date(currentYear, 11, 31), 100)
-console.log(MAX_DATE)
 
 export { DATE_FORMAT, MAX_DATE }
